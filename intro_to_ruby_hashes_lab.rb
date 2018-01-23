f1751ee90e0d053977f6eb6195760014c7d4e311
York Wang <yw544@nyu.edu>
2018-01-23 02:54:23 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -46,7 +46,7 @@ def monopoly_with_fourth_tier
 			names: {
 				reading_railroad: {"mortgage_value" => "$100"},
 				pennsylvania_railroad: {"mortgage_value" => "$200"},
-				b_and_o_railroad: {"mortgage_value" => "$400" },
+				b_and_o_railroad: {"mortgage_value" => "$400"},
 				shortline: {"mortgage_value" => "$800"}
 			},
 			rent_in_dollars: {
